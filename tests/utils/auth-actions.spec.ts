c7fe0c6d01c96f496d711a9934995992176dd7f1
Joshua <joshuaocohen@gmail.com>
2026-04-04 22:00:24 -0500

    auth actions error fix #1: edited navButton to a more robust locator and increased timeout

--- tests/utils/auth-actions.spec.ts
+++ tests/utils/auth-actions.spec.ts
@@ -2,8 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('test authentication', async ({ page }) => {
     await page.goto('/')
+
     // check if user is authenticated:
-    await page.locator('div').filter({ hasText: /^Toggle navigation menu$/ }).locator('div').getByRole('button').click()
+    const navButton = page.getByRole('button', { name: /toggle navigation/i });  
+    await navButton.click({ timeout: 60000 }); // increased timeout for 60 seconds to allow for authentication process to complete
+
     const welcomeMessage = page.getByText('Welcome!')
     await expect(welcomeMessage).toBeVisible()
     // page is authenticated
